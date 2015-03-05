586949b8df362e9fed215811adf6f9e153a354cb
tom <tom@Tom-Netbook>
2015-03-05 17:04:50 +0100

    Improved CMake setup.

---
+++ cmake/FindTengom.cmake
@@ -0,0 +1,33 @@
+SET( TENGOM_PATH "" CACHE PATH "Path of the tengom library" ) 
+
+FIND_PATH(
+    TENGOM_INCLUDE_DIR
+    NAMES tgCore/Version.hpp
+    PATHS ${TENGOM_PATH}
+    PATH_SUFFIXES include
+    DOC "Tengom include directory"
+)
+
+FIND_LIBRARY(
+    TENGOM_CORE
+    NAMES libtgCore.a
+    PATHS ${TENGOM_PATH}/tgCore
+)
+
+FIND_LIBRARY(
+    TENGOM_MATH
+    NAMES libtgMath.a
+    PATHS ${TENGOM_PATH}/tgMath
+)
+
+FIND_LIBRARY(
+    TENGOM_NET
+    NAMES libtgNet.a
+    PATHS ${TENGOM_PATH}/tgNet
+)
+
+SET( TENGOM_FULL
+    ${TENGOM_CORE}
+    ${TENGOM_NET}
+    ${TENGOM_MATH}
+)
---
+++ src/tgCore/Version.hpp
@@ -0,0 +1,1 @@
+#define TENGOM_VERSION 0.1
