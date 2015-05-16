954c9a5b9cdc47eb4ce2e237c58bbf026c676bbe
tom <tom@Tom-Netbook>
2015-05-16 22:35:37 +0200

    Made network library compile under linux again.

--- src/tgNet/Listener.cpp
+++ src/tgNet/Listener.cpp
@@ -2,63 +2,7 @@
 
 #include <tgNet/TCPSocket.hpp>
 
-using namespace tgNet;
-
 #ifdef WIN32
-
-    Listener::Listener( uint16_t port )
-    {
-        // Create socket.
-        m_handle = ::socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
-
-        if( m_handle == INVALID_SOCKET )
-        {
-            auto error = WSAGetLastError();
-            throw std::runtime_error( "Unable to create socket. (" + std::to_string( error ) + ")" );
-        }
-
-        // Initialize address structure.
-        sockaddr_in sockAddr;
-
-        sockAddr.sin_family = AF_INET;
-        sockAddr.sin_port = htons( port );
-        sockAddr.sin_addr.S_un.S_addr = INADDR_ANY;
-
-        // Bind port to socket.
-        if( bind( m_handle, reinterpret_cast< sockaddr* >( &sockAddr ), sizeof( sockAddr ) ) )
-        {
-            auto error = WSAGetLastError();
-            closesocket( m_handle );
-            throw std::runtime_error( "Unable to bind port. (" + std::to_string( error ) + ")" );
-        }
-
-        // Set socket to listening mode.
-        if( listen( m_handle, 10 ) )
-        {
-            auto error = WSAGetLastError();
-            closesocket( m_handle );
-            throw std::runtime_error( "Unable to listen on port. (" + std::to_string( error ) + ")" );
-        }
-    }
-
-    Listener::~Listener()
-    {
-        closesocket( m_handle );
-    }
-
-    std::unique_ptr< TCPSocket > Listener::accept()
-    {
-        auto socket = ::accept( m_handle, NULL, NULL );
-
-        if( socket == INVALID_SOCKET )
-        {
-            auto error = WSAGetLastError();
-            throw std::runtime_error( "Error while waiting for connections. (" + std::to_string( error ) + ")" );
-        }
-
-        return std::unique_ptr< TCPSocket >( new TCPSocket( socket ) );
-    }
-
 #else
 
     #include <sys/socket.h>
@@ -66,44 +10,117 @@ using namespace tgNet;
     #include <sys/types.h>
     #include <netdb.h>
     #include <unistd.h>
-    #include <stdio.h>
 
-    Listener::Listener( int port )
+#endif
+
+using namespace tgNet;
+
+namespace tgNet
+{
+    Listener::Listener( uint16_t port )
     {
-        // create socket
-        m_handle = ::socket( AF_INET, SOCK_STREAM, 0 );
-        if( m_handle == -1 )
-            throw Error::CREATE;
+        #ifdef WIN32
 
-        // build connection
-        struct sockaddr_in sockAddr;
-        sockAddr.sin_family = AF_INET;
-        sockAddr.sin_port = htons( port );
-        sockAddr.sin_addr.s_addr = INADDR_ANY;
-        if( bind( m_handle, reinterpret_cast< struct sockaddr* >( &sockAddr ), sizeof( sockAddr ) ) )
-        {
-            close( m_handle );
-            throw Error::BIND;
-        }
+            // Create socket.
+            m_handle = ::socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
 
-        if( listen( m_handle, 10 ) )
-        {
-            close( m_handle );
-            throw Error::LISTEN;
-        }
+            if( m_handle == INVALID_SOCKET )
+            {
+                auto error = WSAGetLastError();
+                throw std::runtime_error( "Unable to create socket. (" + std::to_string( error ) + ")" );
+            }
+
+            // Initialize address structure.
+            sockaddr_in sockAddr;
+
+            sockAddr.sin_family = AF_INET;
+            sockAddr.sin_port = htons( port );
+            sockAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+            // Bind port to socket.
+            if( bind( m_handle, reinterpret_cast< sockaddr* >( &sockAddr ), sizeof( sockAddr ) ) )
+            {
+                auto error = WSAGetLastError();
+                closesocket( m_handle );
+                throw std::runtime_error( "Unable to bind port. (" + std::to_string( error ) + ")" );
+            }
+
+            // Set socket to listening mode.
+            if( listen( m_handle, 10 ) )
+            {
+                auto error = WSAGetLastError();
+                closesocket( m_handle );
+                throw std::runtime_error( "Unable to listen on port. (" + std::to_string( error ) + ")" );
+            }
+
+        #else
+
+            // Create socket.
+            m_handle = ::socket( AF_INET, SOCK_STREAM, 0 );
+
+            if( m_handle == -1 )
+                throw std::runtime_error( "Unable to create socket." );
+
+            // Initialize address structure.
+            sockaddr_in sockAddr;
+
+            sockAddr.sin_family = AF_INET;
+            sockAddr.sin_port = htons( port );
+            sockAddr.sin_addr.s_addr = INADDR_ANY;
+
+            // Bind port to socket.
+            if( bind( m_handle, reinterpret_cast< sockaddr* >( &sockAddr ), sizeof( sockAddr ) ) )
+            {
+                close( m_handle );
+                throw std::runtime_error( "Unable to bind port." );
+            }
+
+            // Set socket to listening mode.
+            if( listen( m_handle, 10 ) )
+            {
+                close( m_handle );
+                throw std::runtime_error( "Unable to listen on port." );
+            }
+
+        #endif
     }
 
     Listener::~Listener()
     {
-        close( m_handle );
+        #ifdef WIN32
+
+            closesocket( m_handle );
+
+        #else
+
+            close( m_handle );
+
+        #endif
     }
 
     std::unique_ptr< TCPSocket > Listener::accept()
     {
-        int socket = ::accept( m_handle, NULL, NULL );
-        if( socket == -1 )
-            throw Error::ACCEPT;
-        return std::unique_ptr< TCPSocket >( new TCPSocket( socket ) );
-    }
+        #ifdef WIN32
 
-#endif
+            auto socket = ::accept( m_handle, NULL, NULL );
+
+            if( socket == INVALID_SOCKET )
+            {
+                auto error = WSAGetLastError();
+                throw std::runtime_error( "Error while waiting for connections. (" + std::to_string( error ) + ")" );
+            }
+
+            return std::unique_ptr< TCPSocket >( new TCPSocket( socket ) );
+
+        #else
+
+            auto socket = ::accept( m_handle, NULL, NULL );
+
+            if( socket == -1 )
+                throw std::runtime_error( "Error while waiting for connections." );
+
+            return std::unique_ptr< TCPSocket >( new TCPSocket( socket ) );
+
+        #endif
+    }
+}
--- src/tgNet/TCPSocket.cpp
+++ src/tgNet/TCPSocket.cpp
@@ -33,12 +33,28 @@ namespace
         hostent* host = gethostbyname( address.c_str() );
         if( !host )
         {
-            auto error = WSAGetLastError();
-            throw std::runtime_error( "Unable to resolve host name. (" + std::to_string( error ) + ")" );
+            #ifdef WIN32
+
+                auto error = WSAGetLastError();
+                throw std::runtime_error( "Unable to resolve host name. (" + std::to_string( error ) + ")" );
+
+            #else
+
+                throw std::runtime_error( "Unable to resolve host name." );
+
+            #endif
         }
 
         // Copy first valid address to the address structure.
-        sockAddr.sin_addr.S_un.S_addr = *reinterpret_cast< unsigned long* >( host->h_addr_list[ 0 ] );
+        #ifdef WIN32
+
+            sockAddr.sin_addr.S_un.S_addr = *reinterpret_cast< unsigned long* >( host->h_addr_list[ 0 ] );
+
+        #else
+
+            sockAddr.sin_addr.s_addr = *reinterpret_cast< unsigned long* >( host->h_addr_list[ 0 ] );
+
+        #endif
 
         return sockAddr;
     }    
@@ -73,14 +89,15 @@ TCPSocket::TCPSocket( const std::string& address, uint16_t port )
 
         // create socket
         m_handle = ::socket( AF_INET, SOCK_STREAM, 0 );
+
         if( m_handle == -1 )
-            throw Error::CREATE;
+            throw std::runtime_error( "Unable to create socket." );
 
         sockaddr_in sockAddr = createAddress( address, port );
         if( connect( m_handle, reinterpret_cast< struct sockaddr* >( &sockAddr ), sizeof( sockAddr ) ) )
         {
             close( m_handle );
-            throw Error::CONNECT;
+            throw std::runtime_error( "Unable to connect to remote host." );
         }
 
     #endif
