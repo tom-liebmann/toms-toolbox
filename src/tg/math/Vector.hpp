adef4b7e55dc15201eb16341c5f1e32fb3523076
tom <tom@Tom-Netbook>
2015-06-08 21:56:41 +0200

    Dot product.

--- src/tg/math/Vector.hpp
+++ src/tg/math/Vector.hpp
@@ -24,6 +24,11 @@ namespace tg
     Vector< decltype( T1() * T2() ), D > cross(
         const Vector< T1, D >& lhs,
         const Vector< T2, D >& rhs );
+
+    template< typename T1, typename T2, size_t D >
+    decltype( T1() * T2() ) dot(
+        const Vector< T1, D >& lhs,
+        const Vector< T2, D >& rhs );
 }
 
 
@@ -44,8 +49,21 @@ namespace tg
     {
         T result = 0;
 
-        for( size_t i = 0; i < vec.size; ++i )
+        for( size_t i = 0; i < D; ++i )
             result += vec[ i ] * vec[ i ];
+
+        return result;
+    }
+
+    template< typename T1, typename T2, size_t D >
+    decltype( T1() * T2() ) dot(
+        const Vector< T1, D >& lhs,
+        const Vector< T2, D >& rhs )
+    {
+        decltype( T1() * T2() ) result = 0;
+
+        for( size_t i = 0; i < D; ++i )
+            result += lhs[ i ] * rhs[ i ];
 
         return result;
     }
