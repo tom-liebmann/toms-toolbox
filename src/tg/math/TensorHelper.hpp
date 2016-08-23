7b765340412d53582c98596b5a6e6765a6f36e51
Tom Liebmann <tom@tomputer>
2016-08-23 16:07:52 +0200

    Switched to row major order.

--- src/tg/math/TensorHelper.hpp
+++ src/tg/math/TensorHelper.hpp
@@ -4,14 +4,14 @@
 
 namespace tg_private
 {
-    template< size_t D0, size_t... D >
+    template < size_t D0, size_t... D >
     class pack_product
     {
     public:
         static const size_t value = D0 * pack_product< D... >::value;
     };
 
-    template< size_t D0 >
+    template < size_t D0 >
     class pack_product< D0 >
     {
     public:
@@ -20,22 +20,22 @@ namespace tg_private
 
 
 
-    template< size_t D0, size_t... D >
+    template < size_t D0, size_t... D >
     class arg_product
     {
     public:
-        template< typename T0, typename... T >
+        template < typename T0, typename... T >
         static size_t value( T0 i0, T... i )
         {
-            return i0 + D0 * arg_product< D... >::value( i... );
+            return i0 * pack_product< D... >::value + arg_product< D... >::value( i... );
         }
     };
 
-    template< size_t D0 >
+    template < size_t D0 >
     class arg_product< D0 >
     {
     public:
-        template< typename T0 >
+        template < typename T0 >
         static size_t value( T0 i0 )
         {
             return i0;
