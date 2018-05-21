21d1099f80ab7eb1b58f9d3b5d09e4c649fa41a3
Tom Liebmann <tom@Tomputer>
2018-05-21 13:39:00 +0200

    Fix using directive in header file

--- src/core/include/ttb/core/texture/TextureFactory.hpp
+++ src/core/include/ttb/core/texture/TextureFactory.hpp
@@ -2,17 +2,17 @@
 
 #include <ttb/core/texture/Texture2D.hpp>
 #include <ttb/core/texture/Texture3D.hpp>
-#include <vector>
-#include <memory>
 
-using std::vector;
+#include <memory>
+#include <vector>
+
 
 namespace ttb
 {
     namespace TextureFactory
     {
         std::unique_ptr< Texture2D > loadPNG( std::string const& filename );
-        
-        std::unique_ptr< Texture3D > loadPNGArray( std::vector<std::string> const& filenames );
+
+        std::unique_ptr< Texture3D > loadPNGArray( std::vector< std::string > const& filenames );
     }
 }
