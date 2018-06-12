276c7eaab93871e6c8bc0f8a49aec16c3bf3eb0f
Tom Liebmann <tom@Tomputer>
2018-06-12 17:57:40 +0200

    Add TypedOPacket for easier prefix attachment

--- src/net/include/ttb/net/packets.hpp
+++ src/net/include/ttb/net/packets.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <ttb/net/packets/IPacket.hpp>
 #include <ttb/net/packets/DataIPacket.hpp>
+#include <ttb/net/packets/IPacket.hpp>
 #include <ttb/net/packets/SizedIPacket.hpp>
 
-#include <ttb/net/packets/OPacket.hpp>
 #include <ttb/net/packets/DataOPacket.hpp>
+#include <ttb/net/packets/OPacket.hpp>
 #include <ttb/net/packets/SizedOPacket.hpp>
+#include <ttb/net/packets/TypedOPacket.hpp>
