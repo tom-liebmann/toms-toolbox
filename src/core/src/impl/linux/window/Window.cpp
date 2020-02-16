cf885b08aab602e1473916768fcb729bde6e85c0
Tom Surface <tolipost@web.de>
2020-02-16 11:07:24 +0100

    [~] Re-add core profile requirement

--- src/core/src/impl/linux/window/Window.cpp
+++ src/core/src/impl/linux/window/Window.cpp
@@ -87,10 +87,10 @@ namespace ttb
             glfwSetErrorCallback( callbackErrorGLFW );
 
             // prevent glfw to create a context < 3.3
-            // glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
-            // glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+            glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+            glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
             // glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
-            // glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+            glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 
 #ifndef NDEBUG
             std::cout << "glfw version: " << glfwGetVersionString() << std::endl;
