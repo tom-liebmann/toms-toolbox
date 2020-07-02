e37c3194b9b8f9041f7104c7698cb45db79b8ed2
Tom Liebmann <tom@Tomputer>
2020-07-02 14:00:29 +0200

    [#] Fix uniform stacks and dcel build config

--- src/core/include/ttb/core/uniform/stacks/MatrixStack.hpp
+++ src/core/include/ttb/core/uniform/stacks/MatrixStack.hpp
@@ -26,6 +26,25 @@ namespace ttb
 
         std::stack< ValueType > m_valueStack;
     };
+
+
+    template < typename TType, size_t TRows, size_t TCols >
+    class UniformStack< Matrix< TType, TRows, TCols > >::Handle
+    {
+    public:
+        Handle( Handle const& copy ) = delete;
+        Handle& operator=( Handle const& copy ) = delete;
+
+        ~Handle();
+
+    private:
+        explicit Handle( UniformStack& stack );
+
+        UniformStack& m_stack;
+
+        friend class UniformStack;
+    };
+
 }
 
 
@@ -35,8 +54,23 @@ namespace ttb
     inline typename UniformStack< Matrix< TType, TRows, TCols > >::Handle
         UniformStack< Matrix< TType, TRows, TCols > >::push( ValueType const& value )
     {
-        m_valueStack.push( this->value() * value );
-        return { *this };
+        if( m_valueStack.empty() )
+        {
+            return set( value );
+        }
+        else
+        {
+            return set( this->value() * value );
+        }
+    }
+
+    template < typename TType, size_t TRows, size_t TCols >
+    inline typename UniformStack< Matrix< TType, TRows, TCols > >::Handle
+        UniformStack< Matrix< TType, TRows, TCols > >::set( ValueType const& value )
+    {
+        m_valueStack.push( value );
+
+        return Handle{ *this };
     }
 
     template < typename TType, size_t TRows, size_t TCols >
@@ -50,5 +84,18 @@ namespace ttb
     void UniformStack< Matrix< TType, TRows, TCols > >::apply( GLuint location ) const
     {
         helper::set_uniform< ValueType >::apply( location, value() );
+    }
+
+
+    template < typename TType, size_t TRows, size_t TCols >
+    UniformStack< Matrix< TType, TRows, TCols > >::Handle::~Handle()
+    {
+        m_stack.m_valueStack.pop();
+    }
+
+    template < typename TType, size_t TRows, size_t TCols >
+    UniformStack< Matrix< TType, TRows, TCols > >::Handle::Handle( UniformStack& stack )
+        : m_stack{ stack }
+    {
     }
 }
