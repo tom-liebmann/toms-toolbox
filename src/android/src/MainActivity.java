e499ffab73290644daf58a4fa7dbbfed54e4e9ad
Tom Liebmann <tolipost@web.de>
2021-05-02 11:55:36 +0200

    [+] Create working APK with build shell script

---
+++ src/android/src/MainActivity.java
@@ -0,0 +1,34 @@
+package com.juicy_trash.pong_royale;
+
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends android.app.Activity
+{
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstance );
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+}
---
+++ src/android/src/MainView.java
@@ -0,0 +1,266 @@
+package com.juicy_trash.pong_royale;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+import android.view.MotionEvent;
+
+import com.juicy_trash.ApplicationLib;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.opengles.GL10;
+
+class MainView extends GLSurfaceView
+{
+    private static final String TAG = "MainView";
+
+    private final Queue< MotionEvent > m_motionEvents = new LinkedList<>();
+    private final Lock m_lock = new ReentrantLock();
+
+    public MainView( Context context )
+    {
+        super( context );
+        init( context, false, 0, 0 );
+    }
+
+    @Override
+    public boolean onTouchEvent( MotionEvent event )
+    {
+        m_lock.lock();
+        m_motionEvents.add( MotionEvent.obtain( event ) );
+        m_lock.unlock();
+        return true;
+    }
+
+    private void handleMotionEvent( MotionEvent event )
+    {
+        final int action = event.getActionMasked();
+
+        switch( action )
+        {
+            case MotionEvent.ACTION_POINTER_DOWN:
+            case MotionEvent.ACTION_DOWN:
+            {
+                final int pointerNr = event.getActionIndex();
+                final int pointerId = event.getPointerId( pointerNr );
+                final float x = event.getX( pointerNr );
+                final float y = event.getY( pointerNr );
+                ApplicationLib.on_pointer_down( pointerId, x, y );
+                break;
+            }
+
+            case MotionEvent.ACTION_POINTER_UP:
+            case MotionEvent.ACTION_UP:
+            {
+                final int pointerNr = event.getActionIndex();
+                final int pointerId = event.getPointerId( pointerNr );
+                final float x = event.getX( pointerNr );
+                final float y = event.getY( pointerNr );
+                ApplicationLib.on_pointer_up( pointerId, x, y );
+                break;
+            }
+
+            case MotionEvent.ACTION_MOVE:
+            {
+                for( int pointerNr = 0; pointerNr < event.getPointerCount(); ++pointerNr )
+                {
+                    final int pointerId = event.getPointerId( pointerNr );
+                    final float x = event.getX( pointerNr );
+                    final float y = event.getY( pointerNr );
+
+                    ApplicationLib.on_pointer_move( pointerId, x, y );
+                }
+                break;
+            }
+        }
+    }
+
+    private void init( Context context, int depth, int stencil )
+    {
+        setPreserveEGLContextOnPause( true );
+
+        /* Setup the context factory for 2.0 rendering.
+         * See ContextFactory class definition below
+         */
+        setEGLContextFactory( new ContextFactory() );
+
+        /* We need to choose an EGLConfig that matches the format of
+         * our surface exactly. This is going to be done in our
+         * custom config chooser. See ConfigChooser class definition
+         * below.
+         */
+
+        setEGLConfigChooser( 8, 8, 8, 8, 16, 0 );
+        // setEGLConfigChooser( translucent ? new ConfigChooser( 8, 8, 8, 8, depth, stencil )
+        //                                  : new ConfigChooser( 5, 6, 5, 0, depth, stencil ) );
+
+        /* Set the renderer responsible for frame rendering */
+        setRenderer( new Renderer() );
+    }
+
+    private static class ContextFactory implements GLSurfaceView.EGLContextFactory
+    {
+        private static final int EGL_CONTEXT_CLIENT_VERSION = 0x3098;
+
+        public EGLContext createContext( EGL10 egl, EGLDisplay display, EGLConfig eglConfig )
+        {
+            Log.w( TAG, "creating OpenGL ES 3.2 ext context" );
+            checkEglError( "Before eglCreateContext", egl );
+            int[] attrib_list = { EGL_CONTEXT_CLIENT_VERSION, 3, EGL10.EGL_NONE };
+            EGLContext context =
+                    egl.eglCreateContext( display, eglConfig, EGL10.EGL_NO_CONTEXT, attrib_list );
+            checkEglError( "After eglCreateContext", egl );
+            return context;
+        }
+
+        public void destroyContext( EGL10 egl, EGLDisplay display, EGLContext context )
+        {
+            egl.eglDestroyContext( display, context );
+        }
+    }
+
+    private static void checkEglError( String prompt, EGL10 egl )
+    {
+        int error;
+        while( ( error = egl.eglGetError() ) != EGL10.EGL_SUCCESS )
+        {
+            Log.e( TAG, String.format( "%s: EGL error: 0x%x", prompt, error ) );
+        }
+    }
+
+    private static class ConfigChooser implements GLSurfaceView.EGLConfigChooser
+    {
+        public ConfigChooser( int r, int g, int b, int a, int depth, int stencil )
+        {
+            mRedSize = r;
+            mGreenSize = g;
+            mBlueSize = b;
+            mAlphaSize = a;
+            mDepthSize = depth;
+            mStencilSize = stencil;
+        }
+
+        /* This EGL config specification is used to specify 2.0 rendering.
+         * We use a minimum size of 4 bits for red/green/blue, but will
+         * perform actual matching in chooseConfig() below.
+         */
+        private static int EGL_OPENGL_ES2_BIT = 4;
+        private static int[] s_configAttribs2 = { EGL10.EGL_RED_SIZE,
+                4,
+                EGL10.EGL_GREEN_SIZE,
+                4,
+                EGL10.EGL_BLUE_SIZE,
+                4,
+                EGL10.EGL_RENDERABLE_TYPE,
+                EGL_OPENGL_ES2_BIT,
+                EGL10.EGL_NONE };
+
+        public EGLConfig chooseConfig( EGL10 egl, EGLDisplay display )
+        {
+
+            /* Get the number of minimally matching EGL configurations
+             */
+            int[] num_config = new int[1];
+            egl.eglChooseConfig( display, s_configAttribs2, null, 0, num_config );
+
+            int numConfigs = num_config[0];
+
+            if( numConfigs <= 0 )
+            {
+                throw new IllegalArgumentException( "No configs match configSpec" );
+            }
+
+            /* Allocate then read the array of minimally matching EGL configs
+             */
+            EGLConfig[] configs = new EGLConfig[numConfigs];
+            egl.eglChooseConfig( display, s_configAttribs2, configs, numConfigs, num_config );
+
+            /* Now return the "best" one
+             */
+            return chooseConfig( egl, display, configs );
+        }
+
+        public EGLConfig chooseConfig( EGL10 egl, EGLDisplay display, EGLConfig[] configs )
+        {
+            for( EGLConfig config : configs )
+            {
+                int d = findConfigAttrib( egl, display, config, EGL10.EGL_DEPTH_SIZE, 0 );
+                int s = findConfigAttrib( egl, display, config, EGL10.EGL_STENCIL_SIZE, 0 );
+
+                // We need at least mDepthSize and mStencilSize bits
+                if( d < mDepthSize || s < mStencilSize )
+                    continue;
+
+                // We want an *exact* match for red/green/blue/alpha
+                int r = findConfigAttrib( egl, display, config, EGL10.EGL_RED_SIZE, 0 );
+                int g = findConfigAttrib( egl, display, config, EGL10.EGL_GREEN_SIZE, 0 );
+                int b = findConfigAttrib( egl, display, config, EGL10.EGL_BLUE_SIZE, 0 );
+                int a = findConfigAttrib( egl, display, config, EGL10.EGL_ALPHA_SIZE, 0 );
+
+                if( r != mRedSize || g != mGreenSize || b != mBlueSize || a != mAlphaSize )
+                    continue;
+
+                return config;
+            }
+
+            return null;
+        }
+
+        private int findConfigAttrib(
+                EGL10 egl, EGLDisplay display, EGLConfig config, int attribute, int defaultValue )
+        {
+            int[] value = new int[1];
+            if( egl.eglGetConfigAttrib( display, config, attribute, value ) )
+            {
+                return value[0];
+            }
+            return defaultValue;
+        }
+
+        protected int mRedSize;
+        protected int mGreenSize;
+        protected int mBlueSize;
+        protected int mAlphaSize;
+        protected int mDepthSize;
+        protected int mStencilSize;
+    }
+
+    private class Renderer implements GLSurfaceView.Renderer
+    {
+        public void onDrawFrame( GL10 gl )
+        {
+            m_lock.lock();
+            while( !m_motionEvents.isEmpty() )
+            {
+                final MotionEvent event = m_motionEvents.poll();
+
+                if( event != null )
+                {
+                    handleMotionEvent( event );
+                }
+            }
+            m_lock.unlock();
+
+            ApplicationLib.step();
+        }
+
+        public void onSurfaceChanged( GL10 gl, int width, int height )
+        {
+            ApplicationLib.on_resize( width, height );
+        }
+
+        public void onSurfaceCreated( GL10 gl, EGLConfig config )
+        {
+            ApplicationLib.init_gl();
+        }
+    }
+}
