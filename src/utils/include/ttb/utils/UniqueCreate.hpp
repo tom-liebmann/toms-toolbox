7fce834b5cb2ba035e7db9498ce71ce5b6782669
Tom Surface <tolipost@web.de>
2021-07-27 14:00:47 +0200

    [#] Add missing namespace

--- src/utils/include/ttb/utils/UniqueCreate.hpp
+++ src/utils/include/ttb/utils/UniqueCreate.hpp
@@ -3,18 +3,21 @@
 #include <memory>
 
 
-template < typename TType >
-class UniqueCreate
+namespace ttb
 {
-public:
+    template < typename TType >
+    class UniqueCreate
+    {
+    public:
+        template < typename... TArgs >
+        static std::unique_ptr< TType > create( TArgs&&... );
+    };
+
+
+    template < typename TType >
     template < typename... TArgs >
-    static std::unique_ptr< TType > create( TArgs&&... );
-};
-
-
-template < typename TType >
-template < typename... TArgs >
-std::unique_ptr< TType > UniqueCreate< TType >::create( TArgs&&... args )
-{
-    return std::unique_ptr< TType >{ new TType{ std::forward< TArgs >( args )... } };
+    std::unique_ptr< TType > UniqueCreate< TType >::create( TArgs&&... args )
+    {
+        return std::unique_ptr< TType >{ new TType{ std::forward< TArgs >( args )... } };
+    }
 }
