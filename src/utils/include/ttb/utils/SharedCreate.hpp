368e0435ac15bac6d802d9c4c5f159bc24fcafa4
Tom Liebmann <tolipost@web.de>
2021-12-16 14:53:58 +0100

    [+] utils: Add SharedCreateInit helper class

--- src/utils/include/ttb/utils/SharedCreate.hpp
+++ src/utils/include/ttb/utils/SharedCreate.hpp
@@ -3,18 +3,45 @@
 #include <memory>
 
 
-template < typename TType >
-class SharedCreate
+namespace ttb
 {
-public:
+    template < typename TType >
+    class SharedCreate
+    {
+    public:
+        template < typename... TArgs >
+        static std::shared_ptr< TType > create( TArgs&&... );
+    };
+
+    template < typename TType >
+    class SharedCreateInit
+    {
+    public:
+        template < typename... TArgs >
+        static std::shared_ptr< TType > create( TArgs&&... );
+    };
+
+}  // namespace ttb
+
+
+namespace ttb
+{
+    template < typename TType >
     template < typename... TArgs >
-    static std::shared_ptr< TType > create( TArgs&&... );
-};
+    std::shared_ptr< TType > SharedCreate< TType >::create( TArgs&&... args )
+    {
+        return std::shared_ptr< TType >{ new TType{ std::forward< TArgs >( args )... } };
+    }
 
 
-template < typename TType >
-template < typename... TArgs >
-std::shared_ptr< TType > SharedCreate< TType >::create( TArgs&&... args )
-{
-    return std::shared_ptr< TType >{ new TType{ std::forward< TArgs >( args )... } };
-}
+
+    template < typename TType >
+    template < typename... TArgs >
+    std::shared_ptr< TType > SharedCreateInit< TType >::create( TArgs&&... args )
+    {
+        auto obj = std::shared_ptr< TType >{ new TType{ std::forward< TArgs >( args )... } };
+        obj->init();
+        return obj;
+    }
+
+}  // namespace ttb
