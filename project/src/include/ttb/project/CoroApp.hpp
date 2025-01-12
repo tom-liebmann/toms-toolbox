ec2217d47097dad4db15d63fa265b68ea1d67b8d
Tom Liebmann <tolipost@web.de>
2025-01-12 13:09:02 +0100

    Add render state to Coro app

--- project/src/include/ttb/project/CoroApp.hpp
+++ project/src/include/ttb/project/CoroApp.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ApplicationProperties.hpp"
+#include <ttb/core/State.hpp>
 #include <ttb/project/Application.hpp>
 #include <ttb/utils/Event.hpp>
 #include <ttb/utils/co/Coroutine.hpp>
@@ -14,17 +15,19 @@ namespace ttb
 
     private:
         virtual auto getProperties() const -> ApplicationProperties override = 0;
-        virtual auto run( int argc, char** argv ) -> co::Coroutine< void > = 0;
-        virtual void draw() const override = 0;
+        virtual auto run( int argc, char** argv, ttb::State& state ) -> co::Coroutine< void > = 0;
+        virtual void draw( ttb::State& state ) const = 0;
 
         virtual void init( int argc, char** argv ) override;
         virtual void destroy() override;
         virtual void update( float frameTime ) override;
+        virtual void draw() const override;
 
         void onEvent( ttb::Event const& event );
 
         float m_frameTime;
         co::Coroutine< void > m_runCoro;
+        std::unique_ptr< ttb::State > m_renderState;
     };
 
 
