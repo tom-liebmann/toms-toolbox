b32915462fdbbafdc5f19285fc637b51fa3222d3
Tom Liebmann <tolipost@web.de>
2025-01-20 21:25:34 +0100

    Add texture slices to texture.hpp header

--- src/core/include/ttb/core/texture.hpp
+++ src/core/include/ttb/core/texture.hpp
@@ -3,4 +3,6 @@
 #include "texture/Texture.hpp"
 #include "texture/Texture2D.hpp"
 // #include "ttb/core/texture/Texture3D.hpp"
+#include "texture/TextureSlice2D.hpp"
+#include "texture/TextureSlice2DBinder.hpp"
 #include "ttb/core/texture/TextureFactory.hpp"
