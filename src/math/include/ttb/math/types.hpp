08df2ec561588763d999ae747155f9e072da2f7c
Tom Liebmann <tolipost@web.de>
2025-01-30 19:20:45 +0100

    math: Add more type definitions

--- src/math/include/ttb/math/types.hpp
+++ src/math/include/ttb/math/types.hpp
@@ -8,9 +8,13 @@
 namespace ttb
 {
     using Range2F = ttb::Range< float, 2 >;
+    using Range2I = ttb::Range< int, 2 >;
+
     using Matrix3F = ttb::Matrix< float, 3, 3 >;
     using Mat3F = ttb::Matrix< float, 3, 3 >;
     using Mat4F = ttb::Matrix< float, 4, 4 >;
+
     using Vec2F = ttb::Vector< float, 2 >;
     using Vec3F = ttb::Vector< float, 3 >;
+    using Vec2I = ttb::Vector< int, 2 >;
 }
